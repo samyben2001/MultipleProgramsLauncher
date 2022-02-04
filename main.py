9b179796a9db31b0ff62612216164f247dccd2e2
Ben Beya Samy <samyben2001@hotmail.com>
2022-02-04 19:41:13 +0100

    first prototype

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from pyshortcuts import make_shortcut
 
 
-def fill_and_create_bat_file_for_shortcut(files_list):
+def fill_and_create_bat_file_for_shortcut(files_list, path_to_save, filename):
     txt = "@echo off"
     for file, file_info in files.items():
         txt += f'\ncd \"{file_info["path"]}\"' \
                    f'\nstart {file_info["exe"]}'
 
-    bat_file = open(f"{bat_directory}\\{file_name}.bat", "w")
+    bat_file = open(f"{path_to_save}\\{filename}.bat", "w")
     bat_file.write(txt)
     bat_file.close()
 
@@ -22,15 +22,15 @@ file1_exe = "Battle.net Launcher.exe"
 file2_exe = "HearthstoneDeckTracker.exe"
 icon_src = r"C:\Users\samyb\Documents\.Projects\Personnels\MultipleProgramsLauncher\shortcuts.ico"
 
-file_name = file1_exe.rpartition(".")[0].replace(" ", "")
+filename = file1_exe.rpartition(".")[0].replace(" ", "")
 
 files["file1"] = {"path": file1_path, "exe": file1_exe}
 files["file2"] = {"path": file2_path, "exe": file2_exe}
 
 # Create and fill .BAT content
-fill_and_create_bat_file_for_shortcut(files)
+fill_and_create_bat_file_for_shortcut(files, bat_directory, filename)
 
-target = rf'C:\Users\samyb\Documents\.Projects\Personnels\MultipleProgramsLauncher\{bat_directory}\{file_name}.bat'
+target = rf'C:\Users\samyb\Documents\.Projects\Personnels\MultipleProgramsLauncher\{bat_directory}\{filename}.bat'
 
 # Create shortcut
-make_shortcut(target, name=file_name, icon=icon_src)
+make_shortcut(target, name=filename, icon=icon_src)
