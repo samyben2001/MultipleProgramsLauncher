4b09600150b871d9346b9e6fd0b57c6c187883eb
Ben Beya Samy <samyben2001@hotmail.com>
2022-02-07 20:59:58 +0100

    split main in 3 files

--- add_file.py
+++ add_file.py
@@ -1,5 +1,19 @@
+from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.stacklayout import StackLayout
+from kivy.uix.widget import Widget
+from kivy.properties import ColorProperty
+
+Builder.load_file("add_file.kv")
 
 
-class AddFileWidget(StackLayout):
-    pass
+class InfoAndAddButton(FloatLayout):
+    background_color_original = [1.0, 1.0, 1.0, 1.0]
+    background_color_on_click = [0, 1.0, 0, 1.0]
+    background_color = ColorProperty(background_color_original)
+
+    def on_add_more_file_click(self):
+        self.background_color = self.background_color_on_click
+    def on_add_more_file_release(self):
+        self.background_color = self.background_color_original
+
---
+++ shortcut_creation.py
@@ -0,0 +1,8 @@
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+
+Builder.load_file("shortcut_creation.kv")
+
+
+class ShortcutCreationWidget(BoxLayout):
+    pass
