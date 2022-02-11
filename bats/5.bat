d8550fc688a057ba4b3a50b573d9c1fa3ba108cb
Ben Beya Samy <samyben2001@hotmail.com>
2022-02-11 12:26:28 +0100

    a

--- bats/5.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start "" "C:\Users\samyb\AppData\Roaming\Spotify\Spotify.exe"
-start "" "C:\Program Files (x86)\Windscribe\WindscribeLauncher.exe"
--- bats/7.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start "" "C:\Users\samyb\AppData\Local\HearthstoneDeckTracker\HearthstoneDeckTracker.exe"
-start "" "C:\Users\samyb\AppData\Roaming\Spotify\Spotify.exe"
