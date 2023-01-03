6d5c43b4eba7adb21588b2eb927f0551e6fd7ad8
Ben Beya Samy <samyben2001@hotmail.com>
2023-01-03 15:07:19 +0100

    change main

--- kivy_app/main.py
+++ kivy_app/main.py
@@ -5,13 +5,16 @@ Config.set('graphics', 'height', '600')
 Config.set('graphics', 'minimum_width', '1000')
 Config.set('graphics', 'minimum_height', '600')
 Config.set('graphics', 'resizable', '0')
+
+import os, sys
+from kivy.resources import resource_add_path
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ListProperty
 
-Builder.load_file("add_file.kv")
-Builder.load_file("shortcut_creation.kv")
+Builder.load_file("C:\\Users\\samyb\\Documents\\.Projects\\Personnels\\MPC\\MultipleProgramsLauncher\\kivy_app\\add_file.kv")
+Builder.load_file("C:\\Users\\samyb\\Documents\\.Projects\\Personnels\\MPC\\MultipleProgramsLauncher\\kivy_app\\shortcut_creation.kv")
 
 
 class MainWidget(BoxLayout):
@@ -23,4 +26,11 @@ class MultipleProgramsLauncherApp(MDApp):
 
 
 if __name__ == '__main__':
-    MultipleProgramsLauncherApp().run()
+    try:
+        if hasattr(sys, '_MEIPASS'):
+            resource_add_path(os.path.join(sys._MEIPASS))
+        app = MultipleProgramsLauncherApp()
+        app.run()
+    except Exception as e:
+        print(e)
+        input("Press enter.")
